9043a16e1fa609eb8001fc413e06d7b7a32d19f5
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-05-29 16:39:27 -0400

    istg if this breaks

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -193,6 +193,7 @@ namespace Utils {
 		CCScene::get()->addChild(newLabel);
 		SceneManager::get()->keepAcrossScenes(newLabel);
 		newLabel->setVisible(true);
+		GameManager::get()->schedule(schedule_selector(ZealousDateAndTimeLabel::update));
 		if (Utils::getBool("logging")) log::info("ZDATL added");
 	}
 
@@ -200,6 +201,7 @@ namespace Utils {
 		auto zdatl = getZDATL();
 		if (!zdatl) return;
 		CCScene::get()->removeChildByID("zealous-date-and-time-label"_spr);
+		GameManager::get()->unschedule(schedule_selector(ZealousDateAndTimeLabel::update));
 		if (Utils::getBool("logging")) log::info("ZDATL removed");
 	}
 
--- src/ZealousDateAndTimeLabel.hpp
+++ src/ZealousDateAndTimeLabel.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
+#include "Utils.hpp"
+
 class ZealousDateAndTimeLabel : public cocos2d::CCLabelBMFont {
 protected:
 	bool init(const char*, const char*);
 
 public:
 	static ZealousDateAndTimeLabel* create(const char*, const char*);
+	void update(float dt) {
+		ZealousDateAndTimeLabel* zdatl = Utils::getZDATL();
+		if (!zdatl) return;
+		if (!Utils::modEnabled()) return zdatl->setVisible(false);
+		Utils::handleZDATL();
+	}
 };
