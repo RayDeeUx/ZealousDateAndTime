3160788ac52d838e210cdd96e8c874de3b3c097d
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-02-20 01:35:56 -0500

    ayo what

--- src/ZealousDateAndTimeLabel.hpp
+++ src/ZealousDateAndTimeLabel.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 class ZealousDateAndTimeLabel : public cocos2d::CCNode {
+protected:
+	bool init(const char*, const char*);
 public:
 	static ZealousDateAndTimeLabel* create(const char*, const char*);
-	bool init(const char*, const char*);
 	void update(float dt) override;
 	void updateWrapper(float dt) {
 		ZealousDateAndTimeLabel::update(dt);
