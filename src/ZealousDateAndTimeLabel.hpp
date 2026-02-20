f980a91c7a133742a86f6d2e68e4ceed3506de59
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-02-20 01:38:04 -0500

    the fuCK??

--- src/ZealousDateAndTimeLabel.hpp
+++ src/ZealousDateAndTimeLabel.hpp
@@ -3,12 +3,11 @@
 class ZealousDateAndTimeLabel : public cocos2d::CCNode {
 protected:
 	bool init(const char*, const char*);
+	cocos2d::CCLabelBMFont* m_actualLabel;
 public:
 	static ZealousDateAndTimeLabel* create(const char*, const char*);
 	void update(float dt) override;
 	void updateWrapper(float dt) {
 		ZealousDateAndTimeLabel::update(dt);
 	}
-
-	cocos2d::CCLabelBMFont* m_actualLabel;
 };
