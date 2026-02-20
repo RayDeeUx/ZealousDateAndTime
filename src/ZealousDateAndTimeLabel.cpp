25740feda5b196c368653dacfc698c17fac0e3e9
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-02-19 23:26:23 -0500

    aaa

--- src/ZealousDateAndTimeLabel.cpp
+++ src/ZealousDateAndTimeLabel.cpp
@@ -7,11 +7,17 @@ using namespace geode::prelude;
 ZealousDateAndTimeLabel* ZealousDateAndTimeLabel::create(const char* content, const char* fontFile) {
 	auto* ret = new ZealousDateAndTimeLabel();
 	if (ret && ret->initWithString(content, fontFile)) {
+		ret->iFuckingHateHavingToDoThis();
 		ret->autorelease();
 		return ret;
 	}
 	delete ret;
 	return nullptr;
+}
+
+void iFuckingHateHavingToDoThis() {
+	if (this->m_pComponentContainer) return;
+	this->m_pComponentContainer = new CCComponentContainer(this);
 }
 
 void ZealousDateAndTimeLabel::update(float dt) {
--- src/ZealousDateAndTimeLabel.hpp
+++ src/ZealousDateAndTimeLabel.hpp
@@ -3,6 +3,7 @@
 class ZealousDateAndTimeLabel : public cocos2d::CCLabelBMFont {
 public:
 	static ZealousDateAndTimeLabel* create(const char*, const char*);
+	void iFuckingHateHavingToDoThis();
 	void update(float dt);
 	void updateWrapper(float dt) {
 		ZealousDateAndTimeLabel::update(dt);
